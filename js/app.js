2994f5640481debdfd82130830a98e8a7c841d8c
harkiratvirdi <harkiratsinghvirdi1@gmail.com>
2021-01-05 21:29:52 -0500

    completed

--- js/app.js
+++ js/app.js
@@ -1,30 +1,89 @@
-// import "./navbar.js";
-import "./carousel.js";
+gsap.registerPlugin(ScrollTrigger);
 
-let mainNav = document.getElementById("js-menu");
-let navBarToggle = document.getElementById("js-navbar-toggle");
-let toggler = document.getElementById("toggler");
+let setMenu = false;
+const navToggle = document.querySelector(".nav-toggle");
+let tl = gsap.timeline();
+const navLinks = document.querySelectorAll(".nav-links");
 
-console.log(toggler);
-navBarToggle.addEventListener("click", function () {
-  mainNav.classList.toggle("active");
-  toggler.classList.toggle("fa-times");
+console.log(navLinks);
+
+navLinks.forEach((navLink) => {
+  navLink.addEventListener("click", () => {
+    setMenu = false;
+    closeMenu();
+  });
 });
 
-let navbar = document.getElementById("js-navbar");
-let navBtn = document.getElementById("js-btn-black");
-window.onscroll = function () {
-  if (
-    document.documentElement.scrollTop >= 200 ||
-    document.body.scrollTop >= 200
-  ) {
-    navbar.classList.add("navbar__sticky");
-    navBtn.classList.remove("cta-btn--white");
-    navBtn.classList.add("cta-btn--black");
+navToggle.addEventListener("click", () => {
+  if (!setMenu) {
+    setMenu = true;
+    openMenu();
+    console.log("clicked open");
   } else {
-    //   navbar.classList.add("nav-transparent");
-    navBtn.classList.add("cta-btn--white");
-    navBtn.classList.remove("cta-btn--black");
-    navbar.classList.remove("navbar__sticky");
+    setMenu = false;
+    console.log("clicked false");
+    closeMenu();
   }
+});
+
+const openMenu = () => {
+  tl.to("nav", 1, {
+    y: "0",
+    ease: "power4",
+  })
+
+    .to(".hamburger-menu span", 0.6, {
+      delay: -1,
+      scaleX: 0,
+      transformOrigin: "50% 0%",
+      ease: "expo.inOut",
+    })
+
+    .to(".hamburger-menu-close", 0.6, {
+      delay: -0.8,
+      css: { display: "block" },
+    });
 };
+
+const closeMenu = () => {
+  tl.to(".hamburger-menu span", 0.6, {
+    delay: -0.6,
+    scaleX: 1,
+    transformOrigin: "50% 0%",
+    ease: "expo.inOut",
+  })
+    .to(".hamburger-menu-close", 0, {
+      delay: -0.1,
+      css: { display: "none" },
+    })
+    .to("nav", 1, {
+      y: "-1000",
+      ease: "power1.inOut",
+    });
+};
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth <= 900 && window.innerWidth >= 850) {
+    location.reload();
+  }
+});
+
+const links = document.querySelectorAll(".nav-links .heading-5");
+const footerLinks = document.querySelectorAll(".footer__link");
+for (const link of links) {
+  link.addEventListener("click", clickHandler);
+}
+for (const link of footerLinks) {
+  link.addEventListener("click", clickHandler);
+}
+
+function clickHandler(e) {
+  e.preventDefault();
+  const href = this.getAttribute("href");
+  const offsetTop = document.querySelector(href).offsetTop;
+
+  scroll({
+    top: offsetTop,
+    behavior: "smooth",
+  });
+}
--- js/smoothScroll.js
+++ js/smoothScroll.js
@@ -1,77 +1,77 @@
-// const scrollVariables = {
-//   current: 0,
-//   target: 0,
-//   ease: 0.065,
-//   rafId: undefined,
-//   containerHeight: undefined,
-//   rafActive: false,
-//   height: 0,
-// };
-// const container = document.querySelector(".full_container");
+const scrollVariables = {
+  current: 0,
+  target: 0,
+  ease: 0.065,
+  rafId: undefined,
+  containerHeight: undefined,
+  rafActive: false,
+  height: 0,
+};
+const container = document.querySelector(".full_container");
 
-// const setBodyHeight = () => {
-//   scrollVariables.height = container.getBoundingClientRect().height;
-//   document.body.style.height = scrollVariables.height + "px";
-// };
+const setBodyHeight = () => {
+  scrollVariables.height = container.getBoundingClientRect().height;
+  document.body.style.height = scrollVariables.height + "px";
+};
 
-// setBodyHeight();
+setBodyHeight();
 
-// window.addEventListener("scroll", function () {
-//   function setTransform(el, transform) {
-//     el.style.transform = transform;
-//     el.style.WebkitTransform = transform;
-//   }
+window.addEventListener("scroll", function () {
+  function setTransform(el, transform) {
+    el.style.transform = transform;
+    el.style.WebkitTransform = transform;
+  }
 
-//   const setupAnimation = () => {
-//     if (scrollVariables.height === 0) {
-//       setBodyHeight();
-//     }
-//     startAnimation();
-//   };
+  const setupAnimation = () => {
+    // if (scrollVariables.height === 0) {
+    // }
+    setBodyHeight();
+    startAnimation();
+  };
 
-//   function startAnimation() {
-//     if (!scrollVariables.rafActive) {
-//       scrollVariables.rafActive = true;
-//       scrollVariables.rafId = requestAnimationFrame(updateAnimation);
-//     }
-//   }
+  function startAnimation() {
+    if (!scrollVariables.rafActive) {
+      scrollVariables.rafActive = true;
+      scrollVariables.rafId = requestAnimationFrame(updateAnimation);
+    }
+  }
 
-//   function updateScroll() {
-//     scrollVariables.target = window.scrollY || window.pageYOffset;
-//     startAnimation();
-//   }
+  function updateScroll() {
+    scrollVariables.target = window.scrollY || window.pageYOffset;
+    startAnimation();
+  }
 
-//   function updateAnimation() {
-//     // Difference between `target` and `current` scroll position
-//     var diff = scrollVariables.target - scrollVariables.current;
-//     // `delta` is the value for adding to the `current` scroll position
-//     // If `diff < 0.1`, make `delta = 0`, so the animation would not be endless
-//     var delta = Math.abs(diff) < 0.1 ? 0 : diff * scrollVariables.ease;
+  function updateAnimation() {
+    // Difference between `target` and `current` scroll position
+    var diff = scrollVariables.target - scrollVariables.current;
+    // `delta` is the value for adding to the `current` scroll position
+    // If `diff < 0.1`, make `delta = 0`, so the animation would not be endless
+    var delta = Math.abs(diff) < 0.1 ? 0 : diff * scrollVariables.ease;
 
-//     if (delta) {
-//       // If `delta !== 0`
-//       // Update `current` scroll position
-//       scrollVariables.current += delta;
-//       // Round value for better performance
-//       scrollVariables.current = parseFloat(scrollVariables.current.toFixed(2));
-//       // Call `update` again, using `requestAnimationFrame`
-//       scrollVariables.rafId = requestAnimationFrame(updateAnimation);
-//     } else {
-//       // If `delta === 0`
-//       // Update `current`, and finish the animation loop
-//       scrollVariables.current = scrollVariables.target;
-//       scrollVariables.rafActive = false;
-//       cancelAnimationFrame(scrollVariables.rafId);
-//     }
+    if (delta) {
+      // If `delta !== 0`
+      // Update `current` scroll position
+      scrollVariables.current += delta;
+      // Round value for better performance
+      scrollVariables.current = parseFloat(scrollVariables.current.toFixed(2));
+      // Call `update` again, using `requestAnimationFrame`
+      scrollVariables.rafId = requestAnimationFrame(updateAnimation);
+    } else {
+      // If `delta === 0`
+      // Update `current`, and finish the animation loop
+      scrollVariables.current = scrollVariables.target;
+      scrollVariables.rafActive = false;
+      cancelAnimationFrame(scrollVariables.rafId);
+    }
 
-//     // Set the CSS `transform` corresponding to the custom scroll effect
-//     setTransform(container, "translateY(" + -scrollVariables.current + "px)");
-//   }
+    // Set the CSS `transform` corresponding to the custom scroll effect
+    setTransform(container, "translateY(" + -scrollVariables.current + "px)");
+  }
 
-//   // Listen for `scroll` event to update `target` scroll position
-// //
-//   window.addEventListener("scroll", updateScroll);
-//   window.addEventListener("resize", setupAnimation);
-//   // Initial setup
-//   setupAnimation();
-// });
+  // Listen for `scroll` event to update `target` scroll position
+  //
+  window.addEventListener("scroll", updateScroll);
+  window.addEventListener("resize", setupAnimation);
+  // Initial setup
+  setupAnimation();
+});
